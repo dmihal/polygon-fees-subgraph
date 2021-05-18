4704f443180b5c5c4c139bd612e2921da9cff166
David Mihal <david@dmihal.com>
2021-05-19 00:52:11 +0100

    Create subgraph for matic fees

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,45 +1,19 @@
 import { BigInt } from "@graphprotocol/graph-ts"
-import { Contract, LogFeeTransfer } from "../generated/Contract/Contract"
-import { ExampleEntity } from "../generated/schema"
+import { LogFeeTransfer } from "../generated/Contract/Contract"
+import { Fee } from "../generated/schema"
+
+let baseUnit = BigInt.fromI32(10).pow(18).toBigDecimal()
 
 export function handleLogFeeTransfer(event: LogFeeTransfer): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+  let entity = Fee.load('1')
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
   if (entity == null) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
+    entity = new Fee('1')
 
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+    entity.totalFees = BigInt.fromI32(0).toBigDecimal()
   }
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
+  entity.totalFees = entity.totalFees + event.params.amount.divDecimal(baseUnit)
 
-  // Entity fields can be set based on event parameters
-  entity.token = event.params.token
-  entity.from = event.params.from
-
-  // Entities can be written to the store with `.save()`
   entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // None
 }
