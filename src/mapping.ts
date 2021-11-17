ad27cecfd8c5c3f6bd9e46b2085051cce4bfab77
David Mihal <david@dmihal.com>
2021-11-17 22:26:00 +0000

    Use quickswap to internally track MATIC price & USD collected

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,8 +1,11 @@
 import { BigInt } from "@graphprotocol/graph-ts"
 import { LogFeeTransfer } from "../generated/Contract/Contract"
+import { Sync } from "../generated/UniV2Pool/UniV2Pool"
 import { Fee } from "../generated/schema"
 
 let baseUnit = BigInt.fromI32(10).pow(18).toBigDecimal()
+
+let TWELVE_DECIMALS = BigInt.fromI32(10).pow(12)
 
 export function handleLogFeeTransfer(event: LogFeeTransfer): void {
   let entity = Fee.load('1')
@@ -11,9 +14,22 @@ export function handleLogFeeTransfer(event: LogFeeTransfer): void {
     entity = new Fee('1')
 
     entity.totalFees = BigInt.fromI32(0).toBigDecimal()
+    entity.totalFeesUSD = BigInt.fromI32(0).toBigDecimal()
+    entity.maticPrice = BigInt.fromI32(0).toBigDecimal()
   }
 
-  entity.totalFees = entity.totalFees + event.params.amount.divDecimal(baseUnit)
+  entity.totalFees += event.params.amount.divDecimal(baseUnit)
+  entity.totalFeesUSD += event.params.amount.divDecimal(baseUnit).times(entity.maticPrice)
+
+  entity.save()
+}
+
+export function handleQuickswapSync(event: Sync): void {
+  let entity = Fee.load('1')
+
+  entity.maticPrice = event.params.reserve1
+    .times(TWELVE_DECIMALS)
+    .divDecimal(event.params.reserve0.toBigDecimal())
 
   entity.save()
 }
